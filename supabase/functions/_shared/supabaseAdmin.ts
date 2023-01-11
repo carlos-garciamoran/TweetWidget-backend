6e58cccec7ed306156dbd6370d398a9c2d8eb72d
carlosgmoran <46545682+carlos-garciamoran@users.noreply.github.com>
2023-01-11 22:41:53 +0100

    simplify

--- supabase/functions/_shared/supabaseAdmin.ts
+++ supabase/functions/_shared/supabaseAdmin.ts
@@ -1,11 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const SUPABASE_URL = Deno.env.get('SUPABASE_URL')
-const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')
-
-if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
-  console.error('SUPABASE env variables not found, exiting...')
-  Deno.exit(1)
-}
-
-export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
+export const supabaseClient = createClient(
+  Deno.env.get('SUPABASE_URL') ?? '',
+  Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+)
